ce5501b258dfbd4fb18ab9400da167e9c0a7ddbb
Hexan Rauf <hexanraa@gmail.com>
2022-07-02 15:37:26 +0500

    Added drawing board reset function

--- app.js
+++ app.js
@@ -1,15 +1,5 @@
 let color = 'red';
 
-function changeColor(userChoice) {
-  color = userChoice;
-}
-
-function drawingColor() {
-  color === 'random'
-    ? (color = `hsl(${Math.random() * 360},100%,50%)`)
-    : (this.style.backgroundColor = color);
-}
-
 function boardSize() {
   const input = prompt('Enter the size of Board');
   if (input >= 2 && input <= 100) {
@@ -23,6 +13,8 @@ createBoard(40);
 
 function createBoard(size) {
   const board = document.querySelector('.board');
+  const boxes = board.querySelectorAll('div');
+  boxes.forEach((div) => div.remove());
   board.style.display = 'grid';
   board.style.gridTemplateColumns = `repeat(${size},1fr)`;
   board.style.gridTemplateRows = `repeat(${size},1fr)`;
@@ -33,4 +25,20 @@ function createBoard(size) {
     box.style.backgroundColor = 'white';
     board.appendChild(box);
   }
+}
+
+function changeColor(userChoice) {
+  color = userChoice;
+}
+
+function drawingColor() {
+  color === 'random'
+    ? (color = `hsl(${Math.random() * 360},100%,50%)`)
+    : (this.style.backgroundColor = color);
+}
+
+function reset() {
+  const board = document.querySelector('.board');
+  const boxes = board.querySelectorAll('div');
+  boxes.forEach((div) => (div.style.backgroundColor = 'white'));
 }
